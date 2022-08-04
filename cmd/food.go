1aa47641828d4d82f55ef28b467fa3178c8a8a07
Beauty <koshechka00@list.ru>
2022-08-04 21:08:24 +0300

    lesson2

--- cmd/food.go
+++ cmd/food.go
@@ -1,18 +1,35 @@
 package main
 
-func calculete(inp Input) Output {
-	sum := 0
-	topFood := FoodData{}
-	for _, food := range inp.FoodList {
-		sum += food.Kkal
+func count(inp FoodList) Output {
+	total := 0
+	topKkal := Food{}
+	topNameFoodMap := make(map[string]int)
 
-		if food.Kkal > topFood.Kkal {
-			topFood = food
+	for _, food := range inp.List {
+		total += food.Kkal
+
+		if food.Kkal > topKkal.Kkal {
+			topKkal = food
+		}
+
+		count := topNameFoodMap[food.NameFood]
+		count++
+		topNameFoodMap[food.NameFood] = count
+	}
+
+	topCount := 0
+	topName := ""
+	for name := range topNameFoodMap {
+		if topNameFoodMap[name] > topCount {
+			topCount = topNameFoodMap[name]
+			topName = name
 		}
 	}
 
 	return Output{
-		Sum:     sum,
-		KklaTop: topFood,
+		Total:       total,
+		TopKkal:     topKkal,
+		TopNameFood: topName,
+		MidKkal:     total / len(inp.List),
 	}
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,56 +1,65 @@
+// Что нужно: нужно сделать программу, которая на вход получает список еды, которое я съел за выходные (будет указано название и количество калорий),
+// а на выходе показывает:
+// - сколько я суммарно съел калорий Total
+// - какое блюдо было самым калорийным TopKkal
+// - какое блюдо я ел чаще всегo TopNameFood
+// - средняя калорийность MiddleKkal
+
 package main
 
 import "fmt"
 
-var InputData = Input{
-	FoodList: []FoodData{
+type FoodList struct {
+	List []Food
+}
+
+var ImputData = FoodList{
+	List: []Food{
 		{
-			NameFood: "Vok",
+			NameFood: "bread",
+			Kkal:     350,
+		},
+		{
+			NameFood: "omichka",
+			Kkal:     380,
+		},
+		{
+			NameFood: "bread",
+			Kkal:     350,
+		},
+		{
+			NameFood: "peach",
+			Kkal:     200,
+		},
+		{
+			NameFood: "milk",
+			Kkal:     200,
+		},
+		{
+			NameFood: "chips",
 			Kkal:     400,
 		},
 		{
-			NameFood: "Salat",
-			Kkal:     300,
-		},
-		{
-			NameFood: "IceCreem",
-			Kkal:     500,
+			NameFood: "peanut paste",
+			Kkal:     400,
 		},
 	},
 }
 
-func main() {
-	println("mne ploho")
-
-	result := calculete(InputData)
-
-	fmt.Printf("result %+v", result)
-}
-
-// Что нужно: нужно сделать программу, которая на вход получает список еды, которое я съел за выходные (будет указано название и количество калорий),
-// а на выходе показывает:
-// - сколько я суммарно съел калорий
-// - какое блюдо было самым калорийным
-// - какое блюдо я ел чаще всегo
-type Input struct {
-	FoodList []FoodData
-}
-
-type FoodData struct {
+type Food struct {
 	NameFood string
 	Kkal     int
 }
-
-// NameFood Vok 400 Kkkal
-// NameFood Salat 300 Kkkal
-// NameFood IceCreem 500 Kkkal
-
-// sameKkal 5000
-// KklaTop 500 NameFood IceCreem
-// nil
-
 type Output struct {
-	Sum     int
-	KklaTop FoodData
-	Popular string
+	Total       int
+	TopKkal     Food
+	TopNameFood string
+	MidKkal     int
+}
+
+func main() {
+
+	result := count(ImputData)
+
+	fmt.Printf("%+v", result)
 }
